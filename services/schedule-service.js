c3ad7dd46ebd0ea9d455fb7b4b3d6710888c5c87
Tamas Horvath <horvathtamas22@gmail.com>
2020-08-06 11:52:43 +0200

    sch test

--- services/schedule-service.js
+++ services/schedule-service.js
@@ -61,7 +61,7 @@ async function setBetEndings() {
 async function closeWeek() {
   const scheduleTime = '05 11 * * 2';
 
-  const tempTrigger = '32 * * * *';
+  const tempTrigger = '54 * * * *';
 
   schedule.scheduleJob(tempTrigger, async function() {
     const transaction = new Transaction(true);
