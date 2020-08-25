57eeadb333ca8815013383edceb161b427444d13
Tamas Horvath <horvathtamas22@gmail.com>
2020-08-25 23:01:28 +0200

    objects folder removed

--- objects/Invitation.js
+++
@@ -1,9 +0,0 @@
-class Invitation extends User {
-
-    constructor(username, email, leagueName, inviter, applyLink) {
-        super(username, email);
-        this.leagueName = leagueName,
-        this.inviter = inviter,
-        this.applyLink = applyLink
-    }
-}
